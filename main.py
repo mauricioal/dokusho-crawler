7f22c0f9280ac3fb0be9840b026d26411fd8d7cb
HaileyTQuach <hailey.quach@ibm.com>
2025-03-25 17:27:02 -0400

    Added Optional

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import argparse
 from modules.data_extraction import extract_linkedin_profile
 from modules.data_processing import split_profile_data, create_vector_database, verify_embeddings
 from modules.query_engine import generate_initial_facts, answer_user_query
+from typing import Dict, Any, Optional
 import config
 
 # Set up logging
